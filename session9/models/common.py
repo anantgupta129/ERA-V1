e6022036f984065399fc26a67b66258a37b2ea48
Anant Gupta <anantgupta129@gmail.com>
2023-06-29 21:56:07 +0530

    s9 init

---
+++ session9/models/common.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+from torchinfo import summary
+
+
+class BaseNet(nn.Module):
+    def summarize(self, device: torch.device, input_size: tuple = (1, 3, 32, 32)):
+        print(summary(self.to(device), input_size=input_size))
+
+
+# depthwise separable convolution
+class DepthwiseSeparable(nn.Module):
+    def __init__(self, nin, nout, kernel_size=3, padding=1, bias=False):
+        super(DepthwiseSeparable, self).__init__()
+
+        self.depthwise = nn.Conv2d(
+            nin, nin, kernel_size=kernel_size, padding=padding, groups=nin, bias=bias
+        )
+        self.pointwise = nn.Conv2d(nin, nout, kernel_size=1, bias=bias)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = self.depthwise(x)
+        out = self.pointwise(out)
+        return out
