a5c6c50aa8f8df28f800d2cb9b46f24ab80d9964
Anant Gupta <anantgupta129@gmail.com>
2023-07-08 21:11:07 +0530

    models

--- session10/models/common.py
+++ session10/models/common.py
@@ -6,7 +6,6 @@ from torchinfo import summary
 class BaseNet(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        
+
     def summarize(self, device: torch.device, input_size: tuple = (1, 3, 32, 32)):
         print(summary(self.to(device), input_size=input_size))
-
--- session10/models/custom_resnet.py
+++ session10/models/custom_resnet.py
@@ -6,18 +6,22 @@ from .common import BaseNet
 
 
 class ResBlock(nn.Module):
-    def __init__(self, in_planes: int, out_planes: int, stride: int=1, drop: float = 0) -> None:
+    def __init__(
+        self, in_planes: int, out_planes: int, stride: int = 1, drop: float = 0
+    ) -> None:
         super().__init__()
         self.dropout = nn.Dropout2d(drop)
-        
+
         self.conv1 = nn.Conv2d(
-            in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+            in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
+        )
         self.bn1 = nn.BatchNorm2d(out_planes)
-        
+
         self.conv2 = nn.Conv2d(
-            out_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+            out_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
+        )
         self.bn2 = nn.BatchNorm2d(out_planes)
-        
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.dropout(out)
@@ -25,15 +29,15 @@ class ResBlock(nn.Module):
         out += x
         out = F.relu(out)
         out = self.dropout(out)
-        
+
         return out
-    
+
 
 class CustomResNet(BaseNet):
     def __init__(self, drop: float = 0, num_classes: int = 10) -> None:
         super().__init__()
 
-        # perp layer 
+        # perp layer
         self.perlayer = nn.Sequential(
             nn.Conv2d(3, 64, 3, padding=1, bias=False),
             nn.BatchNorm2d(64),
@@ -46,7 +50,7 @@ class CustomResNet(BaseNet):
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Dropout2d(drop),
-            ResBlock(128, 128, drop=drop)
+            ResBlock(128, 128, drop=drop),
         )
         self.layer2 = nn.Sequential(
             nn.Conv2d(128, 256, 3, padding=1, bias=False),
@@ -61,11 +65,11 @@ class CustomResNet(BaseNet):
             nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.Dropout2d(drop),
-            ResBlock(512, 512, drop=drop)
+            ResBlock(512, 512, drop=drop),
         )
         self.pool = nn.MaxPool2d(4)
         self.out = nn.Conv2d(512, num_classes, 1, bias=False)
-    
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.perlayer(x)
         x = self.layer1(x)
@@ -74,4 +78,3 @@ class CustomResNet(BaseNet):
         x = self.pool(x)
         x = self.out(x)
         return x
-    
