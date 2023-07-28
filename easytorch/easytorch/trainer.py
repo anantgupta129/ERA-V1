e9899985ec6bfc906eef30c0e304b168b07e59ef
Anant Gupta <anantgupta129@gmail.com>
2023-07-28 11:23:18 +0530

    code refactor

--- easytorch/easytorch/trainer.py
+++ easytorch/easytorch/trainer.py
@@ -1,35 +1,12 @@
-from random import randint
-from typing import Any, Union
-
 import albumentations as A
 import matplotlib.pyplot as plt
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from torch_lr_finder import LRFinder
-from torchvision import transforms
 from tqdm import tqdm
 
 
-def plot_sampledata(loader):
-    batch_data, batch_label = next(iter(loader))
-
-    fig = plt.figure()
-
-    for i in range(12):
-        plt.subplot(3, 4, i + 1)
-        plt.tight_layout()
-        plt.imshow(batch_data[i].permute(1, 2, 0).numpy())
-        plt.title(batch_label[i].item())
-        plt.xticks([])
-        plt.yticks([])
-
-    plt.show()
-
-
-def GetCorrectPredCount(pPrediction: torch, pLabels):
+def GetCorrectPredCount(pPrediction: torch, pLabels: torch.Tensor):
     return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
 
 
@@ -165,120 +142,3 @@ class Trainer:
         plt.legend()
 
         plt.show()
-
-
-def find_lr(
-    model: nn.Module,
-    device: torch.device,
-    optimizer: torch.optim.Optimizer,
-    criterion: Any,
-    dataloader: DataLoader,
-):
-    """
-    Finds the learning rate for a given model using the LR Finder technique.
-
-    Args:
-        model (nn.Module): The model to find the learning rate for.
-        device (torch.device): The device to run the model on.
-        optimizer (torch.optim.Optimizer): The optimizer used for training the model.
-        criterion (Any): The loss function used for training the model.
-        dataloader (DataLoader): The data loader used for training the model.
-    """
-
-    # Create an instance of the LR Finder
-    lr_finder = LRFinder(model, optimizer, criterion, device=device)
-
-    # Run the range test to find the optimal learning rate
-    lr_finder.range_test(dataloader, end_lr=10, num_iter=200, step_mode="exp")
-
-    # Plot the loss-learning rate graph for inspection
-    lr_finder.plot()
-
-    # Reset the model and optimizer to their initial state
-    lr_finder.reset()
-
-
-def evaluate_model(model: nn.Module, loader: DataLoader, device: torch.device):
-    cols, rows = 4, 6
-    figure = plt.figure(figsize=(20, 20))
-    for i in range(1, cols * rows + 1):
-        k = np.random.randint(0, len(loader.dataset))  # random points from test dataset
-
-        img, label = loader.dataset[k]  # separate the image and label
-        img = img.unsqueeze(0)  # adding one dimention
-        pred = model(img.to(device))  # Prediction
-
-        figure.add_subplot(rows, cols, i)  # making the figure
-        plt.title(
-            f"Predcited label {pred.argmax().item()}\n True Label: {label}"
-        )  # title of plot
-        plt.axis("off")  # hiding the axis
-        plt.imshow(img.squeeze(), cmap="gray")  # showing the plot
-
-    plt.show()
-
-
-def plot_misclassified(
-    model: Any,
-    data_loader: DataLoader,
-    device: torch.device,
-    transformations: A.Compose,
-    title: str = "Misclassified (pred/ truth)",
-):
-    count = 1
-    no_misclf: int = 10
-    rows, cols = 2, int(no_misclf / 2)
-    figure = plt.figure(figsize=(cols * 3, rows * 3))
-
-    classes = data_loader.dataset.classes
-    dataset = data_loader.dataset.ds
-
-    model = model.to(device)
-    model.eval()
-    with torch.inference_mode():
-        while True:
-            k = randint(0, len(dataset))
-            img, label = dataset[k]
-            img = np.array(img)
-
-            aug_img = transformations(image=img)["image"]
-            pred = model(aug_img.unsqueeze(0).to(device)).argmax().item()  # Prediction
-            if pred != label:
-                figure.add_subplot(rows, cols, count)  # adding sub plot
-                plt.title(f"{classes[pred]} / {classes[label]}")  # title of plot
-                plt.axis("off")
-                plt.imshow(img)
-
-                count += 1
-                if count == no_misclf + 1:
-                    break
-
-    plt.suptitle(title, fontsize=15)
-    plt.show()
-
-
-def per_class_accuracy(model: Any, device: torch.device, data_loader: DataLoader):
-    model = model.to(device)
-    model.eval()
-
-    classes = data_loader.dataset.classes
-    nc = len(classes)
-    class_correct = list(0.0 for i in range(nc))
-    class_total = list(0.0 for i in range(nc))
-    with torch.inference_mode():
-        for data in data_loader:
-            images, labels = data
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images.to(device))
-            _, predicted = torch.max(outputs, 1)
-            c = (predicted == labels).squeeze()
-            for i in range(nc):
-                label = labels[i]
-                class_correct[label] += c[i].item()
-                class_total[label] += 1
-
-    print("[x] Accuracy of ::")
-    for i in range(nc):
-        print(
-            "\t[*] %8s : %2d %%" % (classes[i], 100 * class_correct[i] / class_total[i])
-        )
